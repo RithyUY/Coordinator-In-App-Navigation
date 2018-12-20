6403931d08e2df6ba343b5de5fa2d8d4ff15d31c
Rithy Uy <rithy@workwithmad.com>
2018-12-20 15:25:13 +0700

    Add display team photo

---
+++ app/src/main/java/com/rithyuy/coodinatordemo/adapter/ItemDragCallBack.kt
@@ -0,0 +1,9 @@
+package com.rithyuy.coodinatordemo.adapter
+
+/****
+ *
+ * Created by UY RITHY on 12/20/18.
+ *
+ */
+class ItemDragCallBack {
+}
---
+++ app/src/main/java/com/rithyuy/coodinatordemo/src/projectdetail/displayteam/DisplayTeamActivity.kt
@@ -0,0 +1,9 @@
+package com.rithyuy.coodinatordemo.src.projectdetail.displayteam
+
+/****
+ *
+ * Created by UY RITHY on 12/20/18.
+ *
+ */
+class DisplayTeamActivity {
+}
